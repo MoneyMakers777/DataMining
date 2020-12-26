00ed82095152b424633bc5c74e0bb1a37cb75bc4
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2020-12-26 21:20:57 +0000

    Create restartdockers.sh

---
+++ restartdockers.sh
@@ -0,0 +1,104 @@
+docker restart  docker_2
+docker restart  docker_3
+docker restart  docker_4
+docker restart  docker_5
+docker restart  docker_6
+docker restart  docker_7
+docker restart  docker_8
+docker restart  docker_9
+docker restart  docker_a
+docker restart  docker_b
+docker restart  docker_c
+docker restart  docker_d
+docker restart  docker_e
+docker restart  docker_f
+
+
+docker restart  docker_10
+docker restart  docker_11
+docker restart  docker_12
+docker restart  docker_13
+docker restart  docker_14
+docker restart  docker_15
+docker restart  docker_16
+docker restart  docker_17
+docker restart  docker_18
+docker restart  docker_19
+docker restart  docker_1a
+docker restart  docker_1b
+docker restart  docker_1c
+docker restart  docker_1d
+docker restart  docker_1e
+docker restart  docker_1f
+
+
+docker restart  docker_20
+docker restart  docker_21
+docker restart  docker_22
+docker restart  docker_23
+docker restart  docker_24
+docker restart  docker_25
+docker restart  docker_26
+docker restart  docker_27
+docker restart  docker_28
+docker restart  docker_29
+docker restart  docker_2a
+docker restart  docker_2b
+docker restart  docker_2c
+docker restart  docker_2d
+docker restart  docker_2e
+docker restart  docker_2f
+
+
+docker restart  docker_30
+docker restart  docker_31
+docker restart  docker_32
+docker restart  docker_33
+docker restart  docker_34
+docker restart  docker_35
+docker restart  docker_36
+docker restart  docker_37
+docker restart  docker_38
+docker restart  docker_39
+docker restart  docker_3a
+docker restart  docker_3b
+docker restart  docker_3c
+docker restart  docker_3d
+docker restart  docker_3e
+docker restart  docker_3f
+
+
+docker restart  docker_40
+docker restart  docker_41
+docker restart  docker_42
+docker restart  docker_43
+docker restart  docker_44
+docker restart  docker_45
+docker restart  docker_46
+docker restart  docker_47
+docker restart  docker_48
+docker restart  docker_49
+docker restart  docker_4a
+docker restart  docker_4b
+docker restart  docker_4c
+docker restart  docker_4d
+docker restart  docker_4e
+docker restart  docker_4f
+
+
+docker restart  docker_50
+docker restart  docker_51
+docker restart  docker_52
+docker restart  docker_53
+docker restart  docker_54
+docker restart  docker_55
+docker restart  docker_56
+docker restart  docker_57
+docker restart  docker_58
+docker restart  docker_59
+docker restart  docker_5a
+docker restart  docker_5b
+docker restart  docker_5c
+docker restart  docker_5d
+docker restart  docker_5e
+docker restart  docker_5f
