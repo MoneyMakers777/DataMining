1d11ff8614034eb3bb5f9534fce0702e8a7b5653
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2020-12-27 06:51:35 +0000

    Update AfterSystemRestartScript.sh

--- AfterSystemRestartScript.sh
+++ AfterSystemRestartScript.sh
@@ -17,10 +17,10 @@ wget https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/createdock
 chmod +x createdockers.sh
 /root/createdockers.sh
 rm /root/3proxyvol/cfg/3proxy.3cf
-wget https://buy9hits.com/config/3proxy.3cf
+wget https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/3proxy.3cf
 mv 3proxy.3cf /root/3proxyvol/cfg/3proxy.3cf
 rm /root/3proxyvol/cfg/3proxy.cfg
-wget https://buy9hits.com/config/3proxy.cfg
+wget https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/3proxy.cfg
 mv 3proxy.cfg /root/3proxyvol/cfg/3proxy.cfg
 rm restartdockers.sh
 wget https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/restartdockers.sh
