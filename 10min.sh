0d11127c9fcdda931c034cd4cf1793b74416d270
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2020-12-30 04:31:23 +0000

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -5,3 +5,11 @@ usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{
 if [[ ${usage} -le 60 ]]; then
     /sbin/shutdown -r now
 fi
+
+let upSeconds="$(cat /proc/uptime | grep -o '^[0-9]\+')"
+let hours=hours=$((${upSeconds}/3600))
+#if [ "${hours}" -gt "8" ] && [ $(($RANDOM%3)) == 0 ]
+if [ "${hours}" -gt "2" ] 
+then
+   poweroff
+fi
