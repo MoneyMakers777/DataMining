d8e203415aa1564e6df32cf3c891aae7dee45610
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-02-05 08:08:44 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 export systemID=$systemID
 cd /root
-rm setup_m.sh
+#rm setup_m.sh
 wget https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/setup_m.sh
 chmod +x setup_m.sh
 ./setup_m.sh
