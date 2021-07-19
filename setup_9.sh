c25123aff1bade2d9fb810ce14b8dcd131e182cd
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-07-19 03:18:05 +0000

    Update setup_9.sh

--- setup_9.sh
+++ setup_9.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
+apt install net-tools -f
 echo "export systemID=$systemID
 cd /root
 rm AfterSystemRestartScript.sh
@@ -18,8 +19,7 @@ chmod +x /root/installdocker.sh
 mkdir /root/3proxyvol
 mkdir /root/3proxyvol/cfg
 apt install icewm -y
-#yum -y update || apt update && yum -y install git whiptail || apt install -y git whiptail && cd /root && git clone https://github.com/Buy9hitsDOTcom/9Hits-AutoInstall.git --branch 2-4-5 && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "7bb1440ac55eeb5221d7d68c87d33406" "40" "6" "1" "0" "$systemID" "https://exproxy.buy9hits.com/"
-#yum -y update || apt update && yum -y install git whiptail || apt install -y git whiptail && cd /root && git clone https://github.com/MauroS5/9Hits-AutoInstall.git && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "7bb1440ac55eeb5221d7d68c87d33406" "40" "6" "1" "0" "$systemID" "https://exproxy.buy9hits.com/" "" "allow" "allow" "deny"
+yum -y update || apt update && yum -y install git whiptail || apt install -y git whiptail && cd /root && git clone https://github.com/Buy9hitsDOTcom/9Hits-AutoInstall.git --branch 2-4-5 && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "7bb1440ac55eeb5221d7d68c87d33406" "40" "6" "1" "0" "$systemID" "https://exproxy.buy9hits.com/"
 crontab -l | { cat; echo "@reboot /root/initializeSystem.sh"; } | crontab -
 crontab -l | { cat; echo "*/10 * * * * /root/repeatableCMD.sh"; } | crontab -
 /root/initializeSystem.sh
