72ad539e659d3182a05f79e6f5af2c94eaae71b7
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-07-19 22:56:55 +0000

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-cores=$(nproc)
-load=$(awk '{print $1}'< /proc/loadavg)
-usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
-if [[ ${usage} -le 60 ]]; then
-    /sbin/shutdown -r now
+#cores=$(nproc)
+#load=$(awk '{print $1}'< /proc/loadavg)
+#usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
+#if [[ ${usage} -le 60 ]]; then
+#    /sbin/shutdown -r now
+#fi
+
+docker exec docker_2 wget -q --spider http://ipv6.test-ipv6.com/
+if [ $? -eq 0 ]; then
+    echo OK
+else
+   /sbin/shutdown -P now
 fi
+
 
 let upSeconds="$(cat /proc/uptime | grep -o '^[0-9]\+')"
 let hours=hours=$((${upSeconds}/3600))
