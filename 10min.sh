fd4df4a73b7d485a8d5f923cad6adfc589ad9512
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-07-20 03:12:48 +0000

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -17,7 +17,7 @@ fi
 let upSeconds="$(cat /proc/uptime | grep -o '^[0-9]\+')"
 let hours=hours=$((${upSeconds}/3600))
 #if [ "${hours}" -gt "8" ] && [ $(($RANDOM%3)) == 0 ]
-if [ "${hours}" -gt "24" ] && [ $(($RANDOM%3)) == 0 ]
+if [ "${hours}" -gt "7" ] && [ $(($RANDOM%3)) == 0 ]
 then
    #poweroff
    /sbin/shutdown -P now
