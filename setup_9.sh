bd4780c852cb2902998bde83494f195ee567608a
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-07-24 11:45:36 +0000

    Update setup_9.sh

--- setup_9.sh
+++ setup_9.sh
@@ -31,10 +31,14 @@ case=$(($RANDOM%2))
 if [ $case == 0 ]
 then
    #noproxy
+echo "export sys_type=0" > /root/set-vars.sh
+chmod +x /root/set-vars.sh
    yum -y update || apt update && yum -y install git whiptail || apt install -y git whiptail && cd /root && git clone https://github.com/MauroS5/9Hits-AutoInstall.git && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "7bb1440ac55eeb5221d7d68c87d33406" "20" "6" "1" "0" "$systemID" "" "https://buy9hits.com/dl/latest/9hits-linux-x64.tar.bz2" "allow" "allow" "deny"
 elif [ $case == 1 ]
 then
    #20
+echo "export sys_type=1" > /root/set-vars.sh
+chmod +x /root/set-vars.sh
    yum -y update || apt update && yum -y install git whiptail || apt install -y git whiptail && cd /root && git clone https://github.com/MauroS5/9Hits-AutoInstall.git && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "7bb1440ac55eeb5221d7d68c87d33406" "15" "6" "1" "0" "$systemID" "https://exproxy.buy9hits.com/" "https://buy9hits.com/dl/latest/9hits-linux-x64.tar.bz2" "allow" "allow" "deny"
 elif [ $case == 2 ]
 then
