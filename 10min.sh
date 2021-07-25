763a6305210dbe1a21e1b77002e0c44cc3bb368b
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-07-25 01:00:37 +0000

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -21,18 +21,13 @@ if [ $? -eq 0 ]; then
 else
    /sbin/shutdown -P now
 fi
-else
-docker exec docker_2 wget -q --spider http://ipv6.test-ipv6.com/
-if [ $? -eq 0 ]; then
-   /sbin/shutdown -P now
-fi
 fi
 
 let upSeconds="$(cat /proc/uptime | grep -o '^[0-9]\+')"
 let hours=hours=$((${upSeconds}/3600))
 #if [ "${hours}" -gt "8" ] && [ $(($RANDOM%3)) == 0 ]
-#if [ "${hours}" -gt "0" ]
-if [ "${hours}" -gt "7" ] && [ $(($RANDOM%3)) == 0 ]
+if [ "${hours}" -gt "0" ]
+#if [ "${hours}" -gt "7" ] && [ $(($RANDOM%3)) == 0 ]
 then
    #poweroff
    /sbin/shutdown -P now
