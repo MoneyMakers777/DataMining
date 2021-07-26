86dc13064ee726dea1d5c61b018085654cb539f4
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-07-26 04:19:23 +0000

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#cores=$(nproc)
-#load=$(awk '{print $1}'< /proc/loadavg)
-#usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
-#if [[ ${usage} -le 60 ]]; then
-#    /sbin/shutdown -r now
-#fi
+cores=$(nproc)
+load=$(awk '{print $1}'< /proc/loadavg)
+usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
+if [[ ${usage} -le 60 ]]; then
+    /sbin/shutdown -r now
+fi
 FILE=/root/set-vars.sh
 if test -f "$FILE"; then
     echo OK1
