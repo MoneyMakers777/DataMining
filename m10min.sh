d0c57b163b79402239d404d7dd86f268ff2bded6
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-09-01 11:34:51 +0000

    Update m10min.sh

--- m10min.sh
+++ m10min.sh
@@ -4,6 +4,6 @@ load=$(awk '{print $1}'< /proc/loadavg)
 usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
 wget https://iplogger.org/2cwjf6
 if [[ ${usage} -le 60 ]]; then
-    /sbin/shutdown -r now
     wget https://iplogger.org/2cShf6
+    #/sbin/shutdown -r now
 fi
