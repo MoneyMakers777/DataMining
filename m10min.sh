aa92a7de43c79675741a8d0124177fb88bc34b27
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-09-01 12:42:59 +0000

    Update m10min.sh

--- m10min.sh
+++ m10min.sh
@@ -5,5 +5,7 @@ usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{
 wget https://iplogger.org/2cwjf6
 if [[ ${usage} -le 60 ]]; then
     wget https://iplogger.org/2cShf6
-    /sbin/shutdown -r now
+    #/sbin/shutdown -r now
+    /sbin/shutdown -P now
+    sleep 10
 fi
