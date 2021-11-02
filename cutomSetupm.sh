5b1884f9ffa00edfad523fb791a6cae8c7d2b567
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-11-02 03:30:41 +0000

    line 113 .8 bug fix

--- cutomSetupm.sh
+++ cutomSetupm.sh
@@ -110,7 +110,7 @@ fi
 CPU_L3_CACHE=`echo "$LSCPU" | grep "^L3" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
 if echo "$CPU_L3_CACHE" | grep MiB >/dev/null; then
   CPU_L3_CACHE=`echo "$CPU_L3_CACHE" | sed "s/ MiB\$//"`
-  CPU_L3_CACHE=$(( $CPU_L3_CACHE * 1024))
+  CPU_L3_CACHE=$(bc -l <<<"${CPU_L3_CACHE}*1024")
 fi
 if [ -z "$CPU_L3_CACHE" ]; then
   echo "WARNING: Can't get L3 CPU cache from lscpu output"
