4a3f4400036acb20fe89b65589d5c24fe7d3aac8
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2021-11-02 04:02:09 +0000

    Update cutomSetupm.sh

--- cutomSetupm.sh
+++ cutomSetupm.sh
@@ -110,16 +110,14 @@ fi
 CPU_L3_CACHE=`echo "$LSCPU" | grep "^L3" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
 if echo "$CPU_L3_CACHE" | grep MiB >/dev/null; then
   CPU_L3_CACHE=`echo "$CPU_L3_CACHE" | sed "s/ MiB\$//"`
-  CPU_L3_CACHE=$(bc -l <<<"${CPU_L3_CACHE}*1024")
-  echo $CPU_L3_CACHE
+  CPU_L3_CACHE=$(( 10#$CPU_L3_CACHE * 1024))
 fi
 if [ -z "$CPU_L3_CACHE" ]; then
   echo "WARNING: Can't get L3 CPU cache from lscpu output"
   export CPU_L3_CACHE=2048
 fi
 
-TOTAL_CACHE=$(bc -l <<<"${CPU_THREADS} * ${CPU_L1_CACHE} + ${CPU_SOCKETS} * (${CPU_CORES_PER_SOCKET} * ${CPU_L2_CACHE} + ${CPU_L3_CACHE})")
-echo $TOTAL_CACHE
+TOTAL_CACHE=$(( $CPU_THREADS*$CPU_L1_CACHE + $CPU_SOCKETS * ($CPU_CORES_PER_SOCKET*$CPU_L2_CACHE + $CPU_L3_CACHE)))
 if [ -z $TOTAL_CACHE ]; then
   echo "ERROR: Can't compute total cache"
   exit 1
