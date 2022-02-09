091639c4f3485729878829b0243b5323fbefc9c2
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2022-02-09 07:05:27 +0000

    Update setup_9.sh

--- setup_9.sh
+++ setup_9.sh
@@ -9,11 +9,11 @@ crontab -l | { cat; echo "*/15 * * * * /root/repeatableCMD.sh"; } | crontab -
 
 sysnum=${systemID:0:2}
 if test $((10#$sysnum)) -eq 8 || test $((10#$sysnum)) -eq 14 ; then 
-case=0
+case=1
 elif test $((10#$sysnum)) -lt 15 ; then 
-case=0
+case=1
 else
-case=0
+case=1
 fi
 
 #case=$(($RANDOM%2))
