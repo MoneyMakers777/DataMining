583bfa20e735f67d66a3fc02da496f37af6ca112
MoneyMakers777 <73971537+MoneyMakers777@users.noreply.github.com>
2022-03-02 19:18:07 +0000

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -37,6 +37,7 @@ if [ $? -eq 0 ]; then
     echo OK2
 else
    #/sbin/shutdown -P now
+   wget --inet4-only https://api.buy9hits.com/api/devices/$systemID/DisableDeviceConfige -O systemNetworkConfig.sh
    /sbin/shutdown -r now
    sleep 10
 fi
